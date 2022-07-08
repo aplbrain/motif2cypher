154acd8a936ef5453d8ef36f7579d17a1f4b2d8d
Jordan Matelsky <j6k4m8@gmail.com>
2022-07-08 11:19:05 -0400

    Improve handling of files and flags

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ A command-line executable to convert [DotMotif](https://github.com/aplbrain/dotm
 ```shell
 python3 motif2cypher.py < tri.motif
 ```
+
+## Arguments
+
+| Argument        | Description                                                                                          |
+| --------------- | ---------------------------------------------------------------------------------------------------- |
+| `-f`,`--file`   | The input (`*.motif`) file to read from. If this is not specified, the program will read from stdin. |
+| `-o`,`--output` | The output file to write to. If this is not specified, the program will write to stdout.             |
+| `--neuprint`    | If this is specified, the program will write the output using the neuPrint data schema.              |
--- motif2cypher.py
+++ motif2cypher.py
@@ -1,5 +1,47 @@
 from dotmotif import Motif
 from dotmotif.executors.Neo4jExecutor import Neo4jExecutor
+from dotmotif.executors.NeuPrintExecutor import NeuPrintExecutor
 import sys
+import argparse
 
-print(Neo4jExecutor.motif_to_cypher(Motif(sys.stdin.read())))
+if __name__ == "__main__":
+    """
+    Pass a motif file as an argument to this script as `-f`. If no file
+    is passed, the script will attempt to read from stdin.
+
+    You can also pass `--neuprint` to use the NeuPrint executor.
+
+    """
+    parser = argparse.ArgumentParser(
+        description="Convert a dotmotif motif to a cypher query"
+    )
+    parser.add_argument(
+        "-f", "--file", help="File to read from. If not passed, read from stdin."
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="File to write to. If not specified, output will be written to stdout.",
+    )
+    parser.add_argument(
+        "--neuprint", help="Use the NeuPrint executor", action="store_true"
+    )
+    args = parser.parse_args()
+
+    if args.file:
+        with open(args.file) as f:
+            motif = Motif(f.read())
+    else:
+        motif = Motif(sys.stdin.read())
+
+    if args.neuprint:
+        E = NeuPrintExecutor
+    else:
+        E = Neo4jExecutor
+
+    cypher = E.motif_to_cypher(motif)
+    if args.output:
+        with open(args.output, "w") as f:
+            f.write(cypher)
+    else:
+        print(cypher)
